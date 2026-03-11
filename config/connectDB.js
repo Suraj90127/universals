ad84dba4815091ad394192ca6261705103566284
Suraj90127 <surajsingh26574@gmail.com>
2026-03-11 16:01:47 +0530

    okk

--- config/connectDB.js
+++ config/connectDB.js
@@ -12,10 +12,10 @@ const mysql = require("mysql2/promise");
 
 
 const connection = mysql.createPool({
-  host: "72.61.226.242",
-  user: "newaladin",
-  password: "newaladin",
-  database: "newaladin",
+  host: "195.35.22.205",
+  user: "universals",
+  password: "universals",
+  database: "universals",
   dateStrings: true
 });
 
